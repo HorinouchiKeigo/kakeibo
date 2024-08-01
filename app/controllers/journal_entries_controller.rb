d245fd6cc27d701fdc2d1a7e0c2fc7037539310c
HorinouchiKeigo <keigo.horinouchi@prum.jp>
2024-08-01 13:31:31 +0900

    feat: 仕訳作成用のアクションを追加

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -3,10 +3,29 @@ class JournalEntriesController < ApplicationController
 
   def index
     set_user
-    @journal_entries = @user.journal_entries
-    @journal_entry = JournalEntry.new # indexページで登録も行うため
+    @journal_entries = @user.journal_entries.order(created_at: :desc)
+    @journal_entry = @user.journal_entries.new # indexページで登録も行うため
   end
+
+  def create
+    set_user
+    @journal_entry = @user.journal_entries.new(journal_entry_params)
+
+    if @journal_entry.save
+      flash[:notice] = "仕訳を保存しました"
+      redirect_to journal_entries_path
+    else
+      @journal_entries = @user.journal_entries.order(created_at: :desc)
+      flash.now[:alert] = "仕訳の保存に失敗しました"
+      render :index, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def journal_entry_params
+    params.require(:journal_entry).permit(:credit_title, :debit_title, :amount)
+  end
 
   def set_user
     @user = User.find_by(email: session[:userinfo]["email"])
