0f91634bd5a925f6a1c297dd1fbcd6927116775a
HorinouchiKeigo <keigo.horinouchi@prum.jp>
2024-08-01 11:52:56 +0900

    refactor: dryにするため、ユーザーの設定をメソッド化

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -2,8 +2,13 @@ class JournalEntriesController < ApplicationController
   include Secured
 
   def index
-    @user = User.find_by(email: session[:userinfo]["email"])
+    set_user
     @journal_entries = @user.journal_entries
     @journal_entry = JournalEntry.new # indexページで登録も行うため
+  end
+  private
+
+  def set_user
+    @user = User.find_by(email: session[:userinfo]["email"])
   end
 end
