3e7b722e60fe9ec56d0f503514bbe3a4f35fcc44
HorinouchiKeigo <keigo.horinouchi@prum.jp>
2024-08-01 10:16:42 +0900

    feat: ログインページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "sessions/new" => "sessions#new"
+
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
+  # sessions/newからログアウトすると、なぜかauth/logoutではなくsessions/auth/logoutにリダイレクトされる
+  get 'sessions/auth/logout', to: 'auth0#logout'
 end
