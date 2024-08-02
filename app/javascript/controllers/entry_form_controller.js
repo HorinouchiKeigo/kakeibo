c09b2fe5eba96d3c8c203032e3500dc83f5da7f7
HorinouchiKeigo <keigo.horinouchi@prum.jp>
2024-08-02 14:20:18 +0900

    feat: 簡単なバリデーションを追加

---
+++ app/javascript/controllers/entry_form_controller.js
@@ -0,0 +1,96 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="entry-form"
+export default class extends Controller {
+  static targets = ["creditTitle", "debitTitle", "amount", "submitBtn"];
+
+  connect() {
+    this.submitBtnTarget.disabled = true;
+  }
+
+  validate = (event) => {
+    const targetElement = event.target;
+    const validationResult = this.#handleValidation(targetElement);
+    this.#updateView(targetElement, validationResult);
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+    if (this.#isAllValid()) {
+      event.target.submit();
+    } else {
+      this.#validatesAll();
+    }
+  };
+
+  #validatesAll = () => {
+    const elements = [
+      this.creditTitleTarget,
+      this.debitTitleTarget,
+      this.amountTarget,
+    ];
+    elements.forEach((element) => {
+      const validationResult = this.#handleValidation(element);
+      this.#updateView(element, validationResult);
+    });
+  };
+
+  #handleValidation = (element) => {
+    // 入力フォームの一つ前の要素はラベル
+    const labelText = element.previousElementSibling.innerText;
+    const isInteger = element === this.amountTarget;
+    return this.#validates({ value: element.value, labelText, isInteger });
+  };
+
+  #validates = ({ value, labelText, isInteger = false }) => {
+    // 最小文字数
+    if (value.length <= 0) {
+      return { valid: false, message: `${labelText}を入力してください` };
+    }
+
+    // 最大文字数
+    const maxLength = isInteger ? 9 : 20;
+    const length = new Blob([value]).size;
+
+    if (length > maxLength) {
+      const message = isInteger
+        ? `${labelText}は${maxLength}桁以内で入力してください`
+        : `${labelText}は全角${maxLength / 2}文字以内で入力してください`;
+
+      return { valid: false, message: message };
+    }
+
+    // 正の整数
+    if (isInteger && Math.sign(value) === -1) {
+      return {
+        valid: false,
+        message: `${labelText}は正の値を入力してください`,
+      };
+    }
+
+    return { valid: true, message: "" };
+  };
+
+  #updateView = (element, validationResult) => {
+    const errorContainer = element.nextElementSibling;
+    errorContainer.innerText = validationResult.message;
+
+    // toggle(token, force)のforceにboolを渡すと、追加、削除を制御できる
+    element.classList.toggle("hp_invalid__border", !validationResult.valid);
+
+    const label = element.previousElementSibling;
+    label.classList.toggle("hp_invalid__text", !validationResult.valid);
+
+    this.submitBtnTarget.disabled = !this.#isAllValid();
+  };
+
+  #isAllValid = () => {
+    const validationResults = [
+      this.#handleValidation(this.creditTitleTarget),
+      this.#handleValidation(this.debitTitleTarget),
+      this.#handleValidation(this.amountTarget),
+    ];
+
+    return validationResults.every((result) => result.valid);
+  };
+}
